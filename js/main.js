2f0d6e6cc8babece32de986450928ee955288c26
Felipe Vieira da Rocha <felipevieiradarocha@hotmail.com>
2021-12-09 23:38:53 -0300

    Algumas melhorias

--- js/main.js
+++ js/main.js
@@ -51,6 +51,24 @@ $("#botao-reiniciar").on("click", function() {
 
 /*
 JS puro*/
+
+const frases = [
+    'Eu acredito que às vezes são as pessoas que ninguém espera nada que fazem as coisas que ninguém consegue imaginar.',
+    'Nós só podemos ver um pouco do futuro, mas o suficiente para perceber que há muito a fazer.',
+    'As máquinas me surpreendem muito frequentemente.',
+    'Os computadores são inúteis. Eles só podem dar respostas.',
+    'Para fazer um procedimento recursivo é preciso ter fé.',
+    'Todos neste país deveriam aprender a programar um computador, pois isto ensina a pensar.',
+    'Para entender recursão, devemos primeiro compreender uma recursão.',
+    'Primeiro resolva o problema. Depois escreva o código',
+    'Eu preciso de uma boa nota em Web 2',
+    'Falar é fácil, me mostre o código.'
+];
+
+
+var frase = document.getElementById("frase").innerText;
+var numPalavras = frase.split(" ").length;
+var tamanhoFrase = document.getElementById("tamanho-frase").innerText = numPalavras;
 var campo = document.getElementById("campo-digitacao");
 var tempoJogo = document.getElementById("tempo");
 var tempoInicial = tempoJogo.innerText;
@@ -58,12 +76,12 @@ var reiniciarBtn = document.getElementById("botao-reiniciar");
 var cronometro;
 
 campo.addEventListener("input", function(event) {
-    let frase = campo.value;
-    let nCaracteresDigitados = frase.length;
+    let textoUser = campo.value;
+    let nCaracteresDigitados = textoUser.length;
     let caracteresDigitados = document.getElementById("caracteres-digitados");
     caracteresDigitados.innerText = nCaracteresDigitados;
 
-    document.getElementById("palavras-digitadas").innerText = frase.split(" ").length;
+    document.getElementById("palavras-digitadas").innerText = textoUser.split(" ").length;
 });
 
 
@@ -75,13 +93,15 @@ campo.addEventListener("focus", function() {
         return;
     }
     cronometro = setInterval(function() {
-        let tempoRestante = tempoJogo.innerHTML;
+        let tempoRestante = tempoJogo.innerText;
         if (tempoRestante <= 0) {
             campo.setAttribute("disabled", true);
             clearInterval(cronometro);
+            console.log(calculaScore(frase, campo.value));
+            let pontuacao = calculaScore(frase, campo.value);
             let nome = document.getElementById("nome").value;
             let palavrasDigitadas = document.getElementById("palavras-digitadas").innerText;
-            let pontuacao = palavrasDigitadas/tempoInicial * 60;
+            // let pontuacao = palavrasDigitadas/tempoInicial * 60;
             let tabela = document.getElementById("tabela-resultado");
             let tr = document.createElement("tr");
             let td_user = document.createElement("td");
@@ -111,3 +131,23 @@ reiniciarBtn.addEventListener("click", function() {
     document.getElementById("tempo").innerText = tempoInicial;
     nome.focus();
 });
+
+function calculaScore(frase, inputUsuario) {
+    let score = {
+        "acertos": 0,
+        "erros": 0
+    };
+
+    let fraseArray = frase.split(' ');
+    let inputUsuarioArray = inputUsuario.split(' ');
+
+    for (let i = 0; i < fraseArray.length; i++) {
+        if (fraseArray[i] == inputUsuarioArray[i]) {
+            score.acertos++;
+        } else {
+            score.erros++;
+        }
+    }
+
+    return `${score.acertos} / ${score.erros}`;
+}
